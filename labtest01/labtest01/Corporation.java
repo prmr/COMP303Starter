174340f29354b053b34ff96465a378226fc82a8f
prmr <prmr@gmx.com>
2016-03-31 13:32:44 -0400

    Code for Lab test 4

--- labtest01/labtest01/Corporation.java
+++ labtest01/labtest01/Corporation.java
@@ -18,4 +18,5 @@ public class Corporation
 	{
 		aInventories.put(pInventory.getName(), pInventory);
 	}
+
 }
--- labtest01/labtest01/Driver.java
+++ labtest01/labtest01/Driver.java
@@ -1,5 +1,7 @@
 package labtest01;
 
+import java.util.Random;
+
 /**
  * Utility class with a driver program and some 
  * sample items and inventories.
@@ -19,9 +21,19 @@ public final class Driver
 	 */
 	public static void main(String[] pArgs)
 	{
+		Random random = new Random();
+		Inventory inventory1 = new Inventory("IGA Location 1");
+		Inventory inventory2 = new Inventory("IGA Location 2");
+		Inventory inventory3 = new Inventory("IGA Location 3");
 		for( Item item : ITEMS )
 		{
-			System.out.println(item.getName());
+			inventory1.stock(item, random.nextInt(50));
+			inventory2.stock(item, random.nextInt(50));
+			inventory3.stock(item, random.nextInt(50));
 		}
+		Corporation iga = new Corporation();
+		iga.addInventory(inventory1);
+		iga.addInventory(inventory2);
+		iga.addInventory(inventory3);
 	}
 }
